475bee446dd8bc92b6ef0a1f58dba5f5e8f619c6
Kevin Alavik <kevin@alavik.se>
2026-04-23 13:57:14 +0200

    fix: default to the correct "lyr theme"

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -70,7 +70,7 @@ void lyr_entry(void)
 		framebuffer_request.response->framebuffers[0];
 
 	/* use another theme, dark is the default */
-	lyrterm_apply_theme(&lyrterm_theme_dark);
+	lyrterm_apply_theme(&lyrterm_theme_nord);
 	lyrterm_init(framebuffer->address, framebuffer->width, framebuffer->height,
 				 framebuffer->pitch / 4);
 
